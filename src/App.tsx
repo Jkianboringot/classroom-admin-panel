91d41c1825470fad42f131123eed16f79220f512
jkian <slyrics443@gmail.com>
2026-04-11 20:14:57 +0800

    remove dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ import SubjectsList from "./pages/subjects/list.js";
 import { Layout } from "./components/refine-ui/layout/layout";
 import SubjectsCreate from "./pages/subjects/create.js";
 import SubjectsShow from "./pages/subjects/show.js";
-import Dashboard from "./pages/dashboard.tsx";
 
 import { dataProvider } from "./providers/data";
 import ClassesList from "./pages/classes/list.js";
@@ -140,7 +139,7 @@ function App() {
                     </Authenticated>
                   }
                 >
-                  <Route path="/" element={<Dashboard />} />
+                  {/* <Route path="/" element={<Dashboard />} /> */}
 
                   <Route path="subjects">
                     <Route index element={<SubjectsList />} />
